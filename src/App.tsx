e3e3592dadaf1e47ce737642b9abefe3de0f8529
DHN Chandan <php.chandan@gmail.com>
2022-06-04 06:45:00 +0600

    feat: :sparkles: Dispatch event with payload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import { useMachine } from "@xstate/react";
 import "./App.css";
 import { trafficLightMachine } from "./machines/trafficLightMachine";
+import { pizzaBakingMachine } from "./machines/pizzaBakingMachine";
+import { useEffect } from "react";
 
 function App() {
 	const [current, send] = useMachine(trafficLightMachine);
+	const [currentPizza, sendPizza] = useMachine(pizzaBakingMachine);
+
+	useEffect(() => {
+		console.log("Pizza Baking Machine ==============");
+		// console.log(pizzaBakingMachine.initialState);
+		console.log("Current value:", currentPizza.value);
+		// console.log("Current context:", currentPizza.context);
+		// console.log("Current transitions:", currentPizza.transitions);
+
+		sendPizza({ type: "TYPES_UPDATE", payload: "Chilli" }); // way 1
+		// sendPizza("TYPES_UPDATE", { payload: "Motor" }); // way 2
+
+		sendPizza("NEXT");
+
+		console.log("Pizza Baking Machine ==============");
+	}, [currentPizza]);
 
 	return (
 		<div className="App">
